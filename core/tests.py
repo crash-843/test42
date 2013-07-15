679eab493cc38bdd93629546c170d40a3ca738a9
crash-843 <igor.k.843@gmail.com>
2013-07-15 13:28:58 +0300

    ticket:1 - Add test

--- core/tests.py
+++ core/tests.py
@@ -1,16 +1,23 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.test.client import Client
+from models import Contact
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class ContactTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_contact(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+
+        contact = Contact.objects.get(pk=1)
+        self.assertEqual(contact.first_name, 'Igor')
+        self.assertEqual(contact.last_name, 'Kucher')
+        self.assertEqual(contact.birth_date.strftime("%Y-%m-%d"), '1990-01-15')
+        self.assertEqual(contact.bio, 'biography')
+        self.assertEqual(contact.email, 'igor.k.843@gmail.com')
+        self.assertEqual(contact.jabber, 'crash843@khavr.com')
+        self.assertEqual(contact.info.skype, "iggor_ua")
+        self.assertEqual(contact.info.other_contacts, "other_contacts")

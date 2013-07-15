6f180e883c97efed4bd98c57593f3a5feeb77a75
crash-843 <igor.k.843@gmail.com>
2013-07-15 22:17:05 +0300

    ticket:1 - change Makefile

--- test42/settings.py
+++ test42/settings.py
@@ -64,7 +64,7 @@ MEDIA_URL = ''
 # Example: "/var/www/example.com/static/"
 STATIC_ROOT = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static')
 # STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-# print(STATIC_ROOT)
+
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -82,7 +82,7 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Make this unique, and don't share it with anybody.
@@ -92,7 +92,7 @@ SECRET_KEY = 'wj%-+!(8n3o%ldslnu0ewdbur52%&*e8d+8-m1*fg)hc!k6#42'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-    'django.template.loaders.eggs.Loader',
+    # 'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (

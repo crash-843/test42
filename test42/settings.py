e6b4c85aadc94b6bf3652a4ba6b0cee657e63ab9
crash-843 <igor.k.843@gmail.com>
2013-07-16 19:08:52 +0300

    change db name

--- test42/settings.py
+++ test42/settings.py
@@ -13,7 +13,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'test42_1.db',                      # Or path to database file if using sqlite3.
+        'NAME': 'test42test.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',

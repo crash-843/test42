dc950bb7c02a7437159c8ff992b3581722492cdb
crash-843 <igor.k.843@gmail.com>
2013-07-17 16:39:06 +0300

    ticket:5 - add login/logout

--- test42/urls.py
+++ test42/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^', include('core.urls')),
-
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
     url(r'^admin/', include(admin.site.urls)),
 )

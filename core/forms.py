f02954f0b12bc208a5109ef89942113990c3cc9e
crash-843 <igor.k.843@gmail.com>
2013-07-18 14:48:09 +0300

    ticket:6 - add DateWidget

--- core/forms.py
+++ core/forms.py
@@ -1,7 +1,11 @@
 from django.forms import ModelForm
+from widgets import DateWidget
 from models import Contact
 
 
 class ContactForm(ModelForm):
     class Meta:
         model = Contact
+        widgets = {
+            'birth_date': DateWidget(params="dateFormat: 'yy-mm-dd', changeYear: true"),
+        }
